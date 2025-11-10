bc62636ea2f76712e4e965ea5fcfc81e16921c60
seam820 <contactwithseam@gmail.com>
2025-11-10 10:16:21 +0600

    null-vs-un

---
+++ null-vs-undefined.js
@@ -0,0 +1,21 @@
+//let pakhi;
+//console.log(pakhii); // undefined`
+
+function add(num1, num2){
+    console.log(num1 + num2);
+}
+const result = add(5, 10);
+console.log(result); // undefined
+
+const premik = {name: "smart dude", phone: 4589652};
+console.log(premik.gf); // undefined
+
+let fun = undefined;
+console.log(fun); // undefined
+
+let ages = [2, 5, 9];
+console.log(ages[11]); // undefined
+
+
+
+
